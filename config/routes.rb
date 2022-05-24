5453f242cf964efb3d8af2ca3a59e3d78f621516
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-05-24 19:34:11 -0300

    Redirect routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
+
+  get "code", to: redirect("https://github.com/nabucopena/rails_test")
   
   resources :projects
 
   root "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
