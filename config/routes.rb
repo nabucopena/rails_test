e42b7be65b5ea4ca5b452a182eb9cefd56060a65
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-05-24 19:25:11 -0300

    Homepage on root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get "home", to: "pages#home"
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
+  
   resources :projects
+
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
