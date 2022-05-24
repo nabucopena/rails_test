8110c711af549075a92967115355ccaaf21495ba
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-05-24 19:38:30 -0300

    Redirect to error when the route doesn't exist

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
 
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
+  get "error", to: "pages#error"
 
   get "code", to: redirect("https://github.com/nabucopena/rails_test")
   
   resources :projects
 
   root "pages#home"
+
+  get "*path", to: redirect("/error")
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
