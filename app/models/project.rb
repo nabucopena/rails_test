4bf21d9ba56002197b92a6b852d5ad5610b0ba35
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-06-01 22:56:47 -0300

    blank

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
     after_initialize :set_defaults
 
+    validates_presence_of :title, :description
+
     def set_defaults
         self.percent_complete ||= 0.0
     end
