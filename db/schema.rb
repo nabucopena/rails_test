2e9d4c0bf916ea79f400bffd114528d15d31bf79
Felix Nabucodonosor Peña <nabucopena@gmail.com>
2022-06-04 17:42:55 -0300

    Tasks controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_160605) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_04_202321) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_160605) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed"
+    t.text "task_file"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
